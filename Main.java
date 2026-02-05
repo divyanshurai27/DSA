e3c7af9c353bf180873e1b2e0da4c5e0154770b5
divyanshurai27 <146620517+divyanshurai27@users.noreply.github.com>
2026-02-05 21:59:38 +0530

    BS.3

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static int singleNonDuplicate(int[] nums) {
+        int low = 0;
+        int high = nums.length - 2;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == nums[mid ^ 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return nums[low];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,3,3,4,4,8,8};
+        System.out.println(singleNonDuplicate(nums));
+    }
+}
